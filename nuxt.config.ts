db9daaec6fce263faf70bf6e43001e4183466f26
RAGHAB NEUPANE <neupaneraghab@gmail.com>
2026-06-14 12:35:20 +0545

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
-      appUrl: process.env.NUXT_PUBLIC_APP_URL || 'http://localhost:3000',
-      assetsUrl: process.env.NUXT_PUBLIC_ASSETS_URL || '',
+      apiBase: process.env.NUXT_PUBLIC_API_BASE_URL,
+      appUrl: process.env.NUXT_PUBLIC_APP_URL,
+      assetsUrl: process.env.NUXT_PUBLIC_ASSETS_URL,
     }
   }
 })
